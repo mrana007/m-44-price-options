27c1989639b880bdc6bd769cc250847286fe5907
mrana007 <rana.es.bc@gmail.com>
2023-09-19 03:07:34 +0600

    added barchart for phones components

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import LineChart from './components/LineChart/Linechart'
 import NavBar from './components/NavBar/NavBar'
+import Phones from './components/Phones/Phones'
 import PriceOptions from './components/PriceOptions/PriceOptions'
 // import DaisyNav from './components/DaisyNav/Daisynav'
 
@@ -13,6 +14,7 @@ function App() {
       <h1 className='text-7xl bg-rose-600'>Vite + React</h1>
       <PriceOptions></PriceOptions>
       <LineChart></LineChart>
+      <Phones></Phones>
     </>
   )
 }
