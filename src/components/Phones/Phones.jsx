99429502db4537cbe177daef94a1ecfafa124069
mrana007 <rana.es.bc@gmail.com>
2023-09-19 03:34:56 +0600

    added Audio and Grid spinner from react-loader-spinner

--- src/components/Phones/Phones.jsx
+++ src/components/Phones/Phones.jsx
@@ -1,10 +1,12 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
-import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+import { BarChart, Bar,  XAxis, YAxis, Tooltip } from 'recharts';
+import { Audio, Grid } from  'react-loader-spinner'
 
 
 const Phones = () => {
     const [phones, setPhones] =useState([]);
+    const [loading, setLoading] = useState(true);
 
     useEffect(()=>{
         // fetch('https://openapi.programming-hero.com/api/phones?search=iphone')
@@ -22,10 +24,32 @@ const Phones = () => {
                 return obj;
             })
             setPhones(phonesWithFakeData);
+            setLoading(false);
         })
     },[])
     return (
         <div>
+         {loading && <div>
+            <Audio
+            height = "80"
+            width = "80"
+            radius = "9"
+            color = 'green'
+            ariaLabel = 'three-dots-loading'     
+            wrapperStyle
+            wrapperClass
+            />
+            <Grid
+            height="80"
+            width="80"
+            color="#4fa94d"
+            ariaLabel="grid-loading"
+            radius="12.5"
+            wrapperStyle={{}}
+            wrapperClass=""
+            visible={true}
+            />
+        </div>}
             <h2 className="text-5xl">Phones: {phones.length}</h2>
             <BarChart width={1200} height={400} data={phones}>
                 <Bar dataKey="price" fill="#8884d8" />
