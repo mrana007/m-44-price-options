6738760e975c166c21a534a545d86852dd543317
mrana007 <rana.es.bc@gmail.com>
2023-09-19 00:36:35 +0600

    added feature card dynamically

--- src/components/PriceOptions/PriceOptions.jsx
+++ src/components/PriceOptions/PriceOptions.jsx
@@ -48,11 +48,13 @@ const PriceOptions = () => {
       ]
             
     return (
-        <div>
-            <h2 className="text-5xl">Best Prices in the town</h2>
-            {
-                priceOptions.map(option => <PriceOption key={option.id} option={option}></PriceOption>)
-            }
+        <div className="m-12">
+            <h2 className="text-5xl mb-8">Best Prices in the town</h2>
+            <div className='grid grid-cols-1 md:grid-cols-3 gap-6'>
+                {
+                    priceOptions.map(option => <PriceOption key={option.id} option={option}></PriceOption>)
+                }
+            </div>
         </div>
     );
 };
